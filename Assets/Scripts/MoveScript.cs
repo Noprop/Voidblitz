ce98084b9c2eb6e194e003f2983951717be54c50
Noprop <12greg21@gmail.com>
2025-01-24 09:35:37 +0100

    added scripts to move the player and the enemy ships, created sprites for the ammo

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class MoveScript : MonoBehaviour
 {
     public Vector2 speed = new Vector2(15, 15);
+    public Vector2 direction = new Vector2(-1, 0);
 
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
@@ -10,12 +11,9 @@ public class PlayerScript : MonoBehaviour
     // void Start() {}
 
     void Update() {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
         movement = new Vector2(
-            speed.x * inputX,
-            speed.y * inputY
+            speed.x * direction.x,
+            speed.y * direction.y
         );
     }
 
