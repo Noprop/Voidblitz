6e1f2ebeba1583a06f59c420b6969dfeafc8027e
Noprop <12greg21@gmail.com>
2025-01-28 18:44:59 +0100

    add a game over screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
-{
+public class PlayerScript : MonoBehaviour {
     public Vector2 speed = new Vector2(15, 15);
 
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
     private HealthScript health;
-
+    private GameOver gameOver;
 
     void Start() {
         if (health == null) health = GetComponent<HealthScript>();
+        if (gameOver == null) gameOver = FindFirstObjectByType<GameOver>();
     }
 
+    // shoot via the space button
     void Update() {
         bool shoot = Input.GetKeyDown(KeyCode.Space);
         transform.rotation = Quaternion.Euler(0, 0, -90);
@@ -39,6 +40,7 @@ public class PlayerScript : MonoBehaviour
         rigidbodyComponent.linearVelocity = movement;
     }
 
+    // damage enemies and self on collision and ensure we don't go spinning
     private void OnCollisionEnter2D(Collision2D other) {
         EnemyScript enemy = other.gameObject.GetComponent<EnemyScript>();
         if (enemy != null) {
@@ -46,5 +48,12 @@ public class PlayerScript : MonoBehaviour
             health.hp -= 1;
         }
         transform.rotation = Quaternion.Euler(0, 0, -90);
+    }
+
+    // display buttons on death
+    private void OnDestroy() {
+        var gameOver = FindFirstObjectByType<GameOver>();
+        gameOver.ShowButtons();
+        Time.timeScale = 0f;
     }
 }
